d04e2a69c8195b39cb02840f8e46f312819b441c
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2021-02-02 18:07:03 -0300

    class2 structura de directorios en expo y react native. Primer proyecto en react native con expo

--- class2/App.js
+++ class2/App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link } from 'react-router-native';
+
+import NavigationBar from './components/NavigationBar';
+import About from './screens/About';
+import Home from './screens/Home';
+import Topics from './screens/Topics';
+import Images from './screens/Images';
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NativeRouter>
+        <StatusBar />
+        <View style={styles.container}>
+          <NavigationBar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+          <Route path="/images" component={Images} />
+        </View>
+    </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
